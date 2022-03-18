73cfddbccdaecabbf129ddbdab58d8fba3ae6a57
Théotime Godefroy <theotime.godefroy@etu.univ-nantes.fr>
2022-03-18 14:48:11 +0100

    petits updates et fin commentaires

--- src/jeu.ts
+++ src/jeu.ts
@@ -1,99 +1,42 @@
 /**
- * Enumérations, classes et fonctions nécessaires à la création et gestion de nos cartes
- * 
+ * Fonctions de création et manipulation d'un jeu de cartes
+ *
  * @module
 
  */
 
-/**
- * Implémentation des couleurs des cartes
- */
+ import {Carte} from "../src/carte";
 
-export enum Couleur {
-    pique,
-    carreau,
-    trefle,
-    coeur
+ /**
+  * Fonction qui créer et retourne un jeu de 52 cartes
+  * 
+  * @returns Retourne le jeu de 52 cartes créé
+  */
+
+  export function creerJeu(): Carte[] {
+    let jeu_cartes : Carte[] ;
+    let card: Carte;
+    let i: number=0;
+    let j: number=0;
+    for(i;i<=5;i+=1){
+        for(j;j<=14;j+=1){
+            card.valeur = i;
+            card.couleur = j ;
+            jeu_cartes[jeu_cartes.length+1] = card;
+            }
+        }
+    return jeu_cartes;
 }
 
 /**
- * Caractéristiques d'une carte
+ * Fonction qui retourne la première carte tiré d'un paquet de jeu de cartes
+ * @param jeu_de_cartes Jeu de cartes auquel on va tiré une carte
+ * @returns retourne la carte tiré dans le jeu de carte
  */
 
-export type Carte = {
-    couleur :  Couleur,
-    valeur  :  number
-};
-
-/**
- * Céer une carte de la valeur et couleur voulues
- * @param color Couleur correspondante à la carte
- * @param value Valeur correspondant à la carte
- * @returns Retourne la carte créée selon les paramètres entrés.
- */
-
- export function creerCarte (color : Couleur, value : number): Carte {
+ export function tirerCarte(jeu_de_cartes : Carte[]): Carte{
     let card : Carte ;
-    card.couleur = color;
-    card.valeur = value;
+    card = jeu_de_cartes[0];
+    jeu_de_cartes.splice(0,1);
     return card;
-}
-
-/**
- * Fonction qui affiche la carte voulue
- * 
- * @param card carte que l'on souhaite afficher
- */
-
- export function afficherCarte (card : Carte){
-    let valeurCarte : number = card.valeur;
-    let teteCarte : string;
-    if (valeurCarte == 11){
-        teteCarte= "Valet";
-    }
-    else if (valeurCarte ==12){
-        teteCarte="Dame";
-    }
-    else if (valeurCarte ==13){
-        teteCarte="Roi";
-    }
-    else if (valeurCarte ==14){
-        teteCarte="As";
-    }
-    else{
-        teteCarte= String(valeurCarte);
-    }
-    
-    if(card.couleur==0){
-        console.log(teteCarte , " de ♠️"  );
-    }
-    else if(card.couleur == 1) {
-        console.log(teteCarte , " de ♦️" );
-    }
-    else if(card.couleur == 2) {
-        console.log(teteCarte , " de ♣️" );
-    }
-     else{
-        console.log(teteCarte , " de ♥️" );
-    }
-}
-
-/**
- * Fonction qui compare une première carte à une deuxième
- * 
- * @param carte1 1ere carte à comparer
- * @param carte2 2eme carte à comparer
- * @returns retourne 1, -1 ou 0 si la première carte est plus forte, plus faible ou égal à la deuxième
- */
-
- export function comparerCartes ( carte1 : Carte , carte2 : Carte): number{
-    if (carte1.valeur > carte2.valeur){
-        return 1;
-    }
-    else if (carte1.valeur < carte2.valeur){
-        return -1;
-    }
-    else{
-        return 0;
-    }
 }
